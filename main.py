1b6daf926f48d911616250d603d037722d8a16a7
Josue Cortina Suarez <jcortina781@gmail.com>
2020-06-22 00:11:19 -0400

    feat: Add command #everyone for notifying all users in the chat

--- main.py
+++ main.py
@@ -15,4 +15,18 @@ with TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) as bot:
     async def start(event):
         await event.reply('Hello there, %s!' % event.sender.first_name)
 
+    @bot.on(events.NewMessage(
+        pattern='#everyone$'
+    ))
+    async def everyone(event):
+        sender_id = event.from_id
+        chat = await event.get_chat()
+        all_members = await bot.get_participants(chat)
+
+        users = filter(lambda p: p.id != sender_id and not p.bot, all_members)
+        usernames = list(map(lambda u: f'@{u.username}', users))
+        reply = "\n".join(usernames)
+
+        await event.respond(reply)
+
     bot.run_until_disconnected()
